cf185180f118f1884fe6ffa6a3f88928e15db8bc
Ranga Senal Fernando <ranga.fernando@gmail.com>
2023-08-12 21:57:12 +1000

    implement route entry point

---
+++ broker/cmd/api/main.go
@@ -0,0 +1,15 @@
+package main
+
+// why we need github.com/go-chi/chi/v5 <-- it's a 3rd party routing package
+// any alternative for chi
+
+//----- all the dependent packages
+/*
+	github.com/go-chi/chi/v5
+	github.com/go-chi/chi/v5/middleware
+	github.com/go-chi/cors
+*/
+
+func main() {
+
+}
