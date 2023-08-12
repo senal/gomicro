78db3de554acd24b260b18ee945328ef53fa99f8
Ranga Senal Fernando <ranga.fernando@gmail.com>
2023-08-13 01:10:36 +1000

    Add json helpers

--- broker/cmd/api/handlers.go
+++ broker/cmd/api/handlers.go
@@ -1,23 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
-
-type jsonResponse struct {
-	Error   bool   `json:"error"`
-	Message string `json:"message"`
-	Data    any    `json:"data,omitemtpy"`
-}
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	payload := jsonResponse{
 		Error:   false,
 		Message: "Hello",
 	}
-	out, _ := json.MarshalIndent(payload, "", "\t")
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(out)
-	w.WriteHeader(http.StatusAccepted)
+	_ = app.writeJSON(w, http.StatusAccepted, payload)
 }
