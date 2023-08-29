765402ef1b239870e8dda936052c57004a55c31f
Ranga Senal Fernando <ranga.fernando@gmail.com>
2023-08-29 20:19:15 +1000

    Add more methods to manage log entries in the collection

--- logger/data/models.go
+++ logger/data/models.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -80,4 +81,72 @@ func (l *LogEntry) All() ([]*LogEntry, error) {
 		}
 	}
 	return logs, nil
+}
+
+func (l *LogEntry) Get(id string) (*LogEntry, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	collection := client.Database("logs").Collection("logs")
+
+	docId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		log.Println("Error parsing request document id:", err)
+		return nil, err
+	}
+
+	var entry LogEntry
+	err = collection.FindOne(ctx, bson.M{"_id": docId}).Decode(&entry)
+	if err != nil {
+		log.Println("Error fetching item:", err)
+		return nil, err
+	}
+
+	return &entry, nil
+
+}
+
+func (l *LogEntry) DropCollection() error {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	collection := client.Database("logs").Collection("logs")
+
+	if err := collection.Drop(ctx); err != {
+		log.Println("Error dropoing the logs collection:", err)
+		return err
+	}
+	return nil
+}
+
+func (l *LogEntry) Update() (*mongo.UpdateResult, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	collection := client.Database("logs").Collection("logs")
+
+	docId, err := primitive.ObjectIDFromHex(l.ID)
+	if err != nil {
+		log.Println("Error parsing document id:", err)
+		return nil, err
+	}
+
+	var entry LogEntry
+	result, err := collection.UpdateOne(
+		ctx, 
+		bson.M{"_id": docId},
+		bson.D{
+			{"$set", bson.D{
+				{"name", l.Name},
+				{"data", l.data},
+				{"updated_at", time.Now()},
+			}},
+		},
+	);
+
+	if err != nil {
+		log.Println("Error update entry:", err)
+		return nil, err
+	}
+	return result, nil
 }
